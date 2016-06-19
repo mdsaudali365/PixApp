703e0c1720690946b3e09e47bc9f098fe6b44902
Mohammad Saud Ali <mdsaudali@gmail.com>
2016-06-19 13:16:27 +0100

    Create PixGetterConstants.java

---
+++ src/main/java/com/qminder/PixGetterConstants.java
@@ -0,0 +1,9 @@
+package com.qminder;
+
+public class PixGetterConstants{
+	public static final String clientName = "client_name";
+	public static final String fbId = "fb_id";
+	public static final String fbPassword = "fb_pass";
+	public static final String noOfLoops = "loops";
+	public static final String downloadDir = "download_dir";
+}
